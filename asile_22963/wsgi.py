36d7ebd7afbbb5e2fed126dae63f7ecc71871f2a
Crowdbotics <team@crowdbotics.com>
2020-11-25 03:18:18 +0000

    Update models

--- asile_22963/wsgi.py
+++ asile_22963/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asile_22963.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asile_22963.settings")
 
 application = get_wsgi_application()
